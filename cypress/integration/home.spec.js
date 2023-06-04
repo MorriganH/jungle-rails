b9c66c3c205ae091bd92bcaa586c8d9ab5e12ac1
MorriganH <morriganhennessy@gmail.com>
2023-06-04 17:20:43 -0400

    adds tests for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("example to-do app", () => {
+  it("visits the home page", () => {
+    cy.visit("/");
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
