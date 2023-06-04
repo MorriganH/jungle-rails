1474b432cdc10a9db1d4c3e726ebe1ada669a4e3
MorriganH <morriganhennessy@gmail.com>
2023-06-04 17:55:20 -0400

    adds tests for product page

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -12,4 +12,9 @@ describe("example to-do app", () => {
   it("There are 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
   });
+
+  it("Navigate to product page", () => {
+    cy.get(".products article").first().click();
+    cy.get(".product-detail").should("be.visible");
+  });
 });
